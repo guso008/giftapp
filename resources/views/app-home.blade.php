9842c8a135d1cb32c0544b04d3e0e0b0619ed3aa
guso008 <guso008@gmail.com>
2016-07-08 20:33:49 -0300

    ISSUE:#2 Site test. SOLUTIONS: Modificaciones en test, refactor de las columnas. REVIEWER: Guso Olivares.

--- resources/views/app-home.blade.php
+++ resources/views/app-home.blade.php
@@ -19,28 +19,66 @@
 
     <link rel="import" href="bower_components/paper-icon-button/paper-icon-button.html">
     <link rel="import" href="bower_components/paper-button/paper-button.html">
-    <link rel="import" href="bower_components/paper-styles/demo-pages.html">
-
     <link rel="import" href="bower_components/iron-flex-layout/iron-flex-layout.html">
     <link rel="import" href="bower_components/paper-styles/typography.html">
     <link rel="import" href="bower_components/paper-styles/color.html">
+    <link rel="import" href="bower_components/paper-styles/default-theme.html">
     <link rel="import" href="bower_components/neon-animation/neon-animated-pages.html">
     <link rel="import" href="bower_components/neon-animation/neon-animatable.html">
     <link rel="import" href="bower_components/neon-animation/neon-animations.html">
 
     <style is="custom-style">
-        body {
+        body{
             overflow: hidden;
         @apply(--layout-fullbleed);
         @apply(--layout-horizontal);
         }
 
-        .toolbar {
-            position: relative;
-            padding: 8px;
-            background-color: white;
-            z-index: 12;
-        @apply(--layout-centered);
+        .container {
+            overflow: hidden;
+            @apply(--layout-fullbleed);
+            @apply(--layout-vertival);
+        }
+
+        .column-left[flex-layout]{
+            overflow: hidden;
+            @apply(--layout-fullbleed);
+            @apply(--layout-horizontal);
+        }
+
+        .column-left {
+            overflow: hidden;
+            @apply(--layout-fullbleed);
+            @apply(--layout-horizontal);
+        }
+
+        .column-right {
+            @apply(--layout-vertical);
+            @apply(--layout-flex);
+        }
+
+        .text-home{
+            @apply(--layout-vertical);
+            @apply(--layout-center);
+        }
+
+        .container[flex-layout] {
+        @apply(--layout-fullbleed);
+        @apply(--layout-horizontal);
+        @apply(--layout-flex-2);
+        }
+
+        .paper-font-display1 {
+        @apply(--paper-font-display1);
+        }
+
+        .paper-font-display2 {
+        @apply(--paper-font-display2);
+        }
+
+        paper-icon-button.giant {
+            width: 100px;
+            height: 100px;
         }
 
         neon-animated-pages {
@@ -74,84 +112,87 @@
             background: var(--paper-purple-500);
         }
 
-        .flex-1{
-        @apply(--layout-flex);
-        }
-
-        .flex-2{
-        @apply(--layout-flex-2);
-        }
-
-
-        .container{
-            @apply(--layout-fullbleed);
-            @apply(--layout-vertical);
-        }
-        .container[flex-layout]{
-            @apply(--layout-fullbleed);
-            @apply(--layout-horizontal);
-        }
-
-        paper-icon-button.giant {
-            width: 100px;
-            height: 100px;
-        }
     </style>
 
 </head>
 
-<body  >
-
-
+<body>
 <template is="dom-bind">
     <iron-media-query query="(min-width: 600px)" query-matches="@{{wide}}"></iron-media-query>
-    <!--<div class="toolbar">
-        <button on-click="_onUpClick">&#8679;</button>
-        <button on-click="_onDownClick">&#8681;</button>
-    </div>-->
 
     <div class="container" flex-layout$="@{{wide}}">
-        <neon-animated-pages id="pages" selected="[[selected]]" entry-animation="[[entryAnimation]]" exit-animation="[[exitAnimation]]"  style="width: 800px;">
+    <!-- ==========================
+       COLUMN LEFT - START
+   =========================== -->
+    <div class="column-left" >
+        <neon-animated-pages id="pages" selected="[[selected]]" entry-animation="[[entryAnimation]]"
+                             exit-animation="[[exitAnimation]]" style="width:900px;">
             <neon-animatable>
                 REGALA
                 </br>
-                <paper-icon-button icon="expand-more" title="wake up" class="giant" on-click="_onDownClick"></paper-icon-button></neon-animatable>
+                <paper-icon-button icon="expand-more" title="wake up" class="giant"
+                                   on-click="_onDownClick"></paper-icon-button>
+            </neon-animatable>
             <neon-animatable>
                 SUMATE YA!
                 </br>
-                <paper-icon-button icon="expand-more" title="wake up" class="giant" on-click="_onDownClick"></paper-icon-button></neon-animatable>
+                <paper-icon-button icon="expand-more" title="wake up" class="giant"
+                                   on-click="_onDownClick"></paper-icon-button>
+            </neon-animatable>
             </neon-animatable>
             <neon-animatable>EN REGALAR!
                 </br>
-                <paper-icon-button icon="expand-more" title="wake up" class="giant" on-click="_onDownClick"></paper-icon-button></neon-animatable></neon-animatable></neon-animatable>
+                <paper-icon-button icon="expand-more" title="wake up" class="giant"
+                                   on-click="_onDownClick"></paper-icon-button>
+            </neon-animatable>
+            </neon-animatable></neon-animatable>
             <neon-animatable>LA NUEVA EXPERIENCIA
                 </br>
-                <paper-icon-button icon="expand-more" title="wake up" class="giant" on-click="_onDownClick"></paper-icon-button></neon-animatable></neon-animatable></neon-animatable>
+                <paper-icon-button icon="expand-more" title="wake up" class="giant"
+                                   on-click="_onDownClick"></paper-icon-button>
+            </neon-animatable>
+            </neon-animatable></neon-animatable>
             <neon-animatable>MEJOR
                 </br>
-                <paper-icon-button icon="expand-more" title="wake up" class="giant" on-click="_onDownClick"></paper-icon-button></neon-animatable></neon-animatable>
+                <paper-icon-button icon="expand-more" title="wake up" class="giant"
+                                   on-click="_onDownClick"></paper-icon-button>
+            </neon-animatable>
+            </neon-animatable>
         </neon-animated-pages>
+    </div>
+    <!-- ==========================
+    COLUMN LEFT - END
+    =========================== -->
+    <!-- ==========================
+       COLUMN RIGHT - START
+   =========================== -->
+    <div class="column-right">
+        <div class="text-home">
+            <div class="paper-font-display1">Unete gratis.</div>
+            <div class="paper-font-display1">Haz muchos amigos.</div>
+            <div class="paper-font-display1">Comienza a regalar</div>
+            <div class="paper-font-display1">Y recibe tus mejores Regalos</div>
+        </div>
         <div>
-        Unete gratis.
-        Haz muchos amigos.
-        Y comienza a regalar y a recibir los mejores REGAGALOS.
-
-        <div class="signin">
-
-            <a tabindex="-1">
-                <paper-button raised>Registrate ahora gratis<iron-icon icon="icons:arrow-drop-down"></iron-icon></paper-button>
-            </a>
+            <div class="text-home signin">
+                <a tabindex="-1">
+                    <paper-button raised>Registrate ahora gratis
+                        <iron-icon icon="icons:arrow-drop-down"></iron-icon>
+                    </paper-button>
+                </a>
+                <!--<a class="btn action" ng-click="expand = true">Sign up now for free<i class="action-btn-icon"></i></a>
 
 
-            <!--<a class="btn action" ng-click="expand = true">Sign up now for free<i class="action-btn-icon"></i></a>
-
-
-            <a class="btn facebook" click-once="rightBar.loginFB(refferer.slug)" processing-text="Please wait...">Sign in with Facebook</a>
-            <a class="btn twitter" click-once="rightBar.loginTwitter(refferer.slug)" processing-text="Please wait...">Sign in with Twitter</a>
-            <a class="btn google" click-once="rightBar.loginGoogle(refferer.slug)" processing-text="Please wait...">Sign in with Google</a>
-            <a class="btn email" href="/login?s=1">Sign in with Email</a>-->
+                <a class="btn facebook" click-once="rightBar.loginFB(refferer.slug)" processing-text="Please wait...">Sign in with Facebook</a>
+                <a class="btn twitter" click-once="rightBar.loginTwitter(refferer.slug)" processing-text="Please wait...">Sign in with Twitter</a>
+                <a class="btn google" click-once="rightBar.loginGoogle(refferer.slug)" processing-text="Please wait...">Sign in with Google</a>
+                <a class="btn email" href="/login?s=1">Sign in with Email</a>-->
+            </div>
         </div>
     </div>
+    <!-- ==========================
+       COLUMN RIGHT - END
+   =========================== -->
     </div>
 </template>
 
@@ -160,25 +201,25 @@
     var scope = document.querySelector('template[is="dom-bind"]');
     scope.selected = 0;
 
-    scope._onPrevClick = function() {
+    scope._onPrevClick = function () {
         this.entryAnimation = 'slide-from-left-animation';
         this.exitAnimation = 'slide-right-animation';
         this.selected = this.selected === 0 ? 4 : (this.selected - 1);
     }
 
-    scope._onNextClick = function() {
+    scope._onNextClick = function () {
         this.entryAnimation = 'slide-from-right-animation';
         this.exitAnimation = 'slide-left-animation';
         this.selected = this.selected === 4 ? 0 : (this.selected + 1);
     }
 
-    scope._onUpClick = function() {
+    scope._onUpClick = function () {
         this.entryAnimation = 'slide-from-top-animation';
         this.exitAnimation = 'slide-down-animation';
         this.selected = this.selected === 4 ? 0 : (this.selected + 1);
     }
 
-    scope._onDownClick = function() {
+    scope._onDownClick = function () {
         this.entryAnimation = 'slide-from-bottom-animation';
         this.exitAnimation = 'slide-up-animation';
         this.selected = this.selected === 0 ? 4 : (this.selected - 1);
